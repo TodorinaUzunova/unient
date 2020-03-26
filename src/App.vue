21e793f5c20b07b455ca4119c701f09dbfeb04ff
TodorinaUzunova <toschkam@abv.bg>
2020-03-26 17:51:55 +0200

    added Vuetify, added Router and routing

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,87 @@
 <template>
   <div id="app">
       <app-navigation></app-navigation>
+
+      <router-view></router-view>
       <!-- <app-home></app-home> -->
-      <!-- <app-event-list></app-event-list> -->
+      <!-- <app-event-list :events="events" ></app-event-list> -->
+         <!-- <app-not-found></app-not-found> -->
+         <!-- <app-event-create></app-event-create> -->
       <!-- <app-event-details></app-event-details> -->
-      <app-register></app-register>
+      <!-- <app-event-edit></app-event-edit> -->
+       <!-- <app-login></app-login> -->
+      <!-- <app-register></app-register> -->
       <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 import AppNavigation from './components/core/Navigation.vue';
-import AppRegister from './components/Register.vue';
+//import AppRegister from './components/authentication/Register.vue';
+//import AppLogin from './components/authentication/Login.vue';
 import AppFooter from './components/core/Footer.vue';
 //import AppHome from './components/Home.vue';
 //import AppEventList from './components/EventList.vue';
+//import AppNotFound from './components/NotFound.vue';
 //import AppEventDetails from './components/EventDetails.vue';
+//import AppEventEdit from './components/EventEdit.vue';
+//import AppEventCreate from './components/EventCreate.vue';
+//import AppProfile from '@/components/user/Profile.vue';
 
 
 
 export default {
   name: 'App',
+  data(){
+    return {events: [
+    {
+      "_id": "5e1ef996b6cb0338c3abf919",
+      "name": "Concert of ROLING STONES",
+      "dateTime":"12/12/2020",
+      "description": "Aliquip in reprehenderit eu exercitation sint sunt dolor ut culpa incididunt dolor nulla. Quis mollit et sit esse est quis nisi velit. Esse minim ad pariatur fugiat. Sint officia magna elit consequat consequat proident irure cupidatat commodo elit culpa mollit veniam amet. Magna culpa aliqua sint voluptate eu duis sint mollit est sit ut fugiat deserunt. Tempor aliqua eiusmod fugiat qui do eu amet dolore sunt. Nostrud est nisi amet enim deserunt et.",
+      "imageURL": "https://www.rollingstone.com/wp-content/uploads/2018/06/led-zeppelin-iv-album-release-review-35c87652-3510-4318-9145-25a24db837f5.jpg",
+    
+    },
+    {
+      "_id": "5e1ef996a74fd9ecac5a8510",
+      "name": "Concert of ADEL",
+       "dateTime": "12/06/2020",
+      "description": "Enim laboris Lorem esse exercitation commodo. Consectetur officia cillum officia minim est mollit dolor do aliquip consectetur incididunt ad veniam consectetur. Excepteur cupidatat pariatur occaecat amet exercitation eu dolore ut. Proident quis ullamco irure culpa cillum nostrud eu et cillum eu. Duis non velit magna mollit ea incididunt. Tempor sint dolore ad cillum incididunt. Nulla consequat voluptate commodo aute ut.",
+      "imageURL": "https://www.rollingstone.com/wp-content/uploads/2018/06/led-zeppelin-iv-album-release-review-35c87652-3510-4318-9145-25a24db837f5.jpg?",
+     
+    },
+    {
+      "_id": "5e1ef99652de0e99f5f93fff",
+      "name": "Concert of BJORN",
+      "dateTime": "12/08/2020",
+      "description": "In dolore enim culpa duis minim qui deserunt aliquip voluptate et ipsum ut eu labore. Nisi deserunt cupidatat reprehenderit irure officia occaecat sint. Quis adipisicing ut ut ipsum exercitation tempor ut aute. Magna occaecat exercitation ea excepteur consequat cillum. Eu aliquip fugiat eiusmod et id veniam aute voluptate voluptate anim aute in sit. Qui do ut ipsum eu occaecat sint sint id ullamco occaecat adipisicing laborum laborum. Ex aliquip reprehenderit cillum officia est nostrud dolor.",
+      "imageURL": "https://www.rollingstone.com/wp-content/uploads/2018/06/led-zeppelin-iv-album-release-review-35c87652-3510-4318-9145-25a24db837f5.jpg"
+    },
+    {
+      "_id": "5e1ef996c17853358e6da155",
+      "name": "Concert of ARIANA GRANDE",
+      "dateTime": "22/05/2020",
+      "description": "Qui est officia consequat dolor qui. Eu dolor magna quis eiusmod commodo reprehenderit cupidatat occaecat nulla. Enim veniam velit incididunt et est duis sit exercitation ad. Mollit occaecat tempor duis nostrud ut irure irure. Laboris exercitation pariatur exercitation laborum voluptate. Mollit Lorem aute reprehenderit fugiat non magna cupidatat proident ut cillum voluptate aliqua ipsum ad. In incididunt id occaecat sunt eiusmod veniam occaecat proident.",
+      "imageURL": "https://www.rollingstone.com/wp-content/uploads/2018/06/led-zeppelin-iv-album-release-review-35c87652-3510-4318-9145-25a24db837f5.jpg",
+    
+    },
+   
+  ]
+  }},
   components: {
     AppNavigation,
-    AppRegister,
+    //AppRegister,
+    //AppLogin,
     AppFooter,
     //AppHome,
     //AppEventList,
+    // AppNotFound,
+    //AppEventCreate,
     //AppEventDetails,
+    //AppEventEdit,
+    //AppProfile
+    
+
   }
 }
 </script>
@@ -171,6 +225,7 @@ form label{
 }
 
 .btn{
+
   width: 25%;
   margin: 0 auto;
   color: white;
@@ -192,11 +247,18 @@ form label{
 }
 
 .alert{
-   width: 50%;
+   width: 100%;
    margin: 0 auto;
    text-align: center;
    margin-top: 1%;
+   margin-bottom: 3%;
 }
+
+.alert-danger{
+  
+  text-decoration-color: red;
+}
+
 
 .imageContainer{
    border:2px solid black;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from '@/plugins/vuetify' // path to vuetify export
+import router from '@/plugins/router' // path to router
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  router,
+  vuetify
 }).$mount('#app')
